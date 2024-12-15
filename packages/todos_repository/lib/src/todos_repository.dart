6a02cdf1fc28c069bcaec52a09d7ea182af79eee
lucas.keterson <lucasketersonsk8@gmail.com>
2024-12-15 20:37:52 -0300

    Project created

---
+++ packages/todos_repository/lib/src/todos_repository.dart
@@ -0,0 +1,8 @@
+/// {@template todos_repository}
+// A repository that handles `todo` related requests.
+/// {@endtemplate}
+library;
+class TodosRepository {
+  /// {@macro todos_repository}
+  const TodosRepository();
+}
---
+++ packages/todos_repository/lib/todos_repository.dart
@@ -0,0 +1,3 @@
+// A repository that handles `todo` related requests.
+
+export 'src/todos_repository.dart';
