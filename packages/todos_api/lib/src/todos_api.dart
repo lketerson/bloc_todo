a81b34f3ea58340757c2af9805d2cb9812978799
tse - Lketerson <lucasketerson@gmail.com>
2024-12-18 17:48:32 -0300

    Adicionado: interface todoApi

--- packages/todos_api/lib/src/todos_api.dart
+++ packages/todos_api/lib/src/todos_api.dart
@@ -1,7 +1,23 @@
+import 'models/todo.dart';
+
 /// {@template todos_api}
 /// The interface and models for an API providing access to todos.
 /// {@endtemplate}
-class TodosApi {
+abstract class TodosApi {
   /// {@macro todos_api}
   const TodosApi();
+
+  Stream<List<Todo>> getTodos();
+
+  Future<void> saveTodo(Todo todo);
+
+  Future<void> deleteTodo(String id);
+
+  Future<int> clearCompleted();
+
+  Future<int> completeAll();
+
+  Future<void> close();
 }
+
+class TodoNotFoundException implements Exception {}
